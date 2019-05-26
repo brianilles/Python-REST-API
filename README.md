8283dd59716e874cc985b0e812773ba95d0ea18f
Brian Illes <brianrilles@gmail.com>
2019-05-26 07:42:16 -0700

    adds GET /todos

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Python-REST-API
-A simple Restful API with Python
+## To Get started
+
+### Follow these steps
+
+Assumes proper Python workspace
+
+1. In base directory, you can run: `python server.py`
+
+2. Open Postman (or similar) to **_http://localhost:5000 [endpoint]_**
+
+| Endpoint     | Description          |
+| ------------ | -------------------- |
+| GET `/test`  | Returns current time |
+| GET `/todos` | Return todos         |
--- server.py
+++ server.py
@@ -1,7 +1,18 @@
-from flask import Flask 
+from datetime import datetime
+from flask import Flask, request 
+from flask_json import FlaskJSON, JsonError, json_response
 
+# Creates Flask instance
 app = Flask(__name__)
+json = FlaskJSON(app)
 
-@app.route("/")
-def hello():
-    return "sanity!"
+@app.route('/')
+def test():
+    now = datetime.now()
+    return json_response(time=now)
+
+@app.route("/todos", methods=["GET"])
+def todos():
+    return json_response(todos=[ {'id':1,'task': 'eat dinner'}, {'id': 2, 'task': 'wash car'}, {'id': 3, 'task': 'make flask app'}])
+
+app.run()
